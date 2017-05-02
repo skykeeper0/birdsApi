394313699db79208799770153668757fa28c035f
skykeeper0 <trungdinh0429@gmail.com>
2017-05-01 22:43:45 -0500

    add AWS DB instance access to connection

--- src/connection/knex.js
+++ src/connection/knex.js
@@ -1,9 +1,9 @@
 module.exports = require('knex')({
   client: 'mysql',
   connection: {
-    host: 'localhost',
-    user: 'skykeeper0',
-    password: '123',
+    host: 'freebird.c7vnc5yar7af.us-west-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'demon895530',
 
     database: 'birdbase',
     charset: 'utf8',
